2667d8e3e0c8fbc31146f36989872d45fbcad20e
Jerry Lerman <lerman@runbox.com>
2018-12-22 19:15:32 -0500

    Add mustache.js for message templates

--- public/js/index.js
+++ public/js/index.js
@@ -17,29 +17,55 @@ socket.on('connect', function () {
 //   console.log('Connected to server');
 // });
 
+// Using Mustache. It lets you inject values
+
 socket.on('disconnect', function () {
   console.log('Disconnected from server');
 });
 
+// Mustache is allowing dynamic valuues to get inserted
 socket.on('newMessage', function (message) {
   var formattedTime = moment(message.createdAt).format('h:mm a');
-  var li = jQuery('<li></li>');
-  li.text(`${message.from} ${formattedTime}: ${message.text}`);
-
-  jQuery('#messages').append(li);
+  // Fetch the template from the html
+  var template = jQuery('#message-template').html();
+  // Fill the template (1st arg) with the data (2nd arg)
+  var html = Mustache.render(template, {
+    text: message.text,
+    from: message.from,
+    createdAt: formattedTime
+  });
+  //Put the rendered output back into the html
+  jQuery('#messages').append(html);
+  // var li = jQuery('<li></li>');
+  // li.text(`${message.from} ${formattedTime}: ${message.text}`);
+  //
+  // jQuery('#messages').append(li);
 });
 
 socket.on('newLocationMessage', function (message) {
   var formattedTime = moment(message.createdAt).format('h:mm a');
-  var li = jQuery('<li></li>');
-  var a = jQuery('<a target="_blank">My current location</a>');
-
-  li.text(`${message.from} ${formattedTime}: `);
-  a.attr('href', message.url);
-  li.append(a);
-
-  jQuery('#messages').append(li);
+  var template = jQuery('#location-message-template').html();
+  var html = Mustache.render(template, {
+    url: message.url,
+    from: message.from,
+    createdAt: formattedTime
+  });
+  jQuery('#messages').append(html);
 });
+
+
+
+// socket.on('newLocationMessage', function (message) {
+//   var formattedTime = moment(message.createdAt).format('h:mm a');
+//   var li = jQuery('<li></li>');
+//   var a = jQuery('<a target="_blank">My current location</a>');
+//
+//   li.text(`${message.from} ${formattedTime}: `);
+//   a.attr('href', message.url);
+//   li.append(a);
+//
+//   jQuery('#messages').append(li);
+// });
 
 jQuery('#message-form').on('submit',function (event) {
   event.preventDefault(); //Stop browser default behavior (refresh and adding something to URL)
